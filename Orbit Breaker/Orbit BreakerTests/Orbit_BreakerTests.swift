f4f1207987a127b512e9320f77c26d027e51806a
August Wetterau <augustwetterau@MacBookPro.lan>
2024-10-25 13:51:18 -0700

    Created Xcode Project

---
+++ Orbit Breaker/Orbit BreakerTests/Orbit_BreakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Orbit_BreakerTests.swift
+//  Orbit BreakerTests
+//
+//  Created by August Wetterau on 10/25/24.
+//
+
+import Testing
+@testable import Orbit_Breaker
+
+struct Orbit_BreakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Orbit Breaker/Orbit BreakerUITests/Orbit_BreakerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Orbit_BreakerUITestsLaunchTests.swift
+//  Orbit BreakerUITests
+//
+//  Created by August Wetterau on 10/25/24.
+//
+
+import XCTest
+
+final class Orbit_BreakerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
