b16e659367466829534d9e6e79772969f2a42516
Thomas Rife <thomasrife@Thomass-MacBook-Pro.local>
2024-11-06 15:18:01 -0800

    commenting up code

--- Orbit Breaker/Orbit Breaker/Player/PlayerPowerUps.swift
+++ Orbit Breaker/Orbit Breaker/Player/PlayerPowerUps.swift
@@ -10,28 +10,28 @@ import SpriteKit
 enum PowerUps: CaseIterable {
     case shield
     case doubleDamage
-    case moreBullets
 }
 
 class PowerUp {
     let type : PowerUps
     private let duration : TimeInterval
     
+    // intitialize all power up traits
     init(type: PowerUps, duration: TimeInterval = 5.0) {
         self.type = type
         self.duration = duration
     }
     
+    // apply to the player ship
     func apply(to player: Player) {
         switch type {
         case .shield:
             player.addShield()
             scheduleEndEffect(for: player, after: duration)
-            // enemy bullets damage = 0
+            // enemy bullets damage = 0 or some other way to implement 0 damage for enemies
         case .doubleDamage:
+            // alter damage multiplier from player class to double the existing 10 damage
             player.damageMultiplier = 2
-        case .moreBullets:
-            break
         }
     }
     
@@ -41,14 +41,14 @@ class PowerUp {
         }
     }
     
+    // removing these power ups after a certain amout of time (5s at the moment)
     private func remove(from Player: Player) {
         switch type {
         case .shield:
             Player.removeShield()
         case .doubleDamage:
             Player.damageMultiplier = 1
-        case .moreBullets:
-            break
+
         }
     }
 }
